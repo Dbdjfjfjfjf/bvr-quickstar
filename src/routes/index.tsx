919c1230f3af977a9423f4c2d31fae930d9ad317
Dbdjfjfjfjf <114397191+Dbdjfjfjfjf@users.noreply.github.com>
2024-07-21 13:34:38 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,11 @@
 import { Title } from "solid-start";
-
+import Counter from "~/components/Counter";
 
 export default function Home() {
   return (
     <main>
       <Title>TESTE</Title>
       <h1>Ruan!</h1>
-      
     </main>
   );
 }
